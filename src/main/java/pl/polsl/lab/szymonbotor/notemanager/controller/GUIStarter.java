73fa2dc52a2f06e1cf1e38d60ce495d559adec6f
sbotor <botorszym@gmail.com>
2021-12-12 17:36:53 +0100

    Added a GUI starter class.

---
+++ src/main/java/pl/polsl/lab/szymonbotor/notemanager/controller/GUIStarter.java
@@ -0,0 +1,10 @@
+package pl.polsl.lab.szymonbotor.notemanager.controller;
+
+import pl.polsl.lab.szymonbotor.notemanager.view.MainFXView;
+
+public class GUIStarter {
+
+    public static void main(String[] args) {
+        MainFXView.main(args);
+    }
+}
--- src/main/java/pl/polsl/lab/szymonbotor/notemanager/controller/PassGenFXController.java
+++ src/main/java/pl/polsl/lab/szymonbotor/notemanager/controller/PassGenFXController.java
@@ -3,7 +3,6 @@ package pl.polsl.lab.szymonbotor.notemanager.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.Clipboard;
