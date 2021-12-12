dbeef7d8fd134f0771accf0adf4d87b2fe26b654
sbotor <botorszym@gmail.com>
2021-12-12 16:56:35 +0100

    Main gui code is done, need to write tests and javadoc comments.

--- src/main/java/pl/polsl/lab/szymonbotor/notemanager/controller/PassGenFXController.java
+++ src/main/java/pl/polsl/lab/szymonbotor/notemanager/controller/PassGenFXController.java
@@ -2,8 +2,12 @@ package pl.polsl.lab.szymonbotor.notemanager.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import pl.polsl.lab.szymonbotor.notemanager.exceptions.InvalidCharacterException;
 import pl.polsl.lab.szymonbotor.notemanager.exceptions.InvalidPasswordLengthException;
 import pl.polsl.lab.szymonbotor.notemanager.model.PasswordGen;
@@ -40,8 +44,23 @@ public class PassGenFXController {
             int passLen = Integer.parseInt(passLenField.getText());
             PasswordGen passGen = new PasswordGen(passLen, symbolStr);
             generatedPassword.setText(passGen.generate());
-        } catch (InvalidPasswordLengthException | InvalidCharacterException e) {
-            e.printStackTrace();
+        } catch (InvalidCharacterException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, e.getMessage());
+            alert.showAndWait();
+        } catch (InvalidPasswordLengthException | NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid password length.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void copyButtonClicked(ActionEvent event) {
+        String password = generatedPassword.getText();
+        if (!password.isEmpty()) {
+            Clipboard clipboard = Clipboard.getSystemClipboard();
+            ClipboardContent content = new ClipboardContent();
+            content.putString(password);
+            clipboard.setContent(content);
         }
     }
 }
