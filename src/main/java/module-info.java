3cd1c341cdec993cd6dd05fba2890a0adfaf2f9d
sbotor <botorszym@gmail.com>
2022-01-11 18:35:07 +0100

    Project cleanup into branches.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module pl.polsl.lab.szymonbotor.notemanager {
     requires javafx.fxml;
     requires java.base;
 
-    requires transitive jakarta.jakartaee.api;
-
     exports pl.polsl.lab.szymonbotor.notemanager.model;
     exports pl.polsl.lab.szymonbotor.notemanager.view;
     opens pl.polsl.lab.szymonbotor.notemanager.view to javafx.fxml;
