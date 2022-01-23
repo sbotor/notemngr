58c48a4395752003267bf08ef3976b13b11615d8
sbotor <botorszym@gmail.com>
2022-01-23 01:59:48 +0100

    Initial database commit.

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pl.polsl.lab.szymonbotor.notemanager {
-    requires transitive jakarta.jakartaee.api;
-
-    exports pl.polsl.lab.szymonbotor.notemanager.model;
-    exports pl.polsl.lab.szymonbotor.notemanager.view;
-    exports pl.polsl.lab.szymonbotor.notemanager.controller;
-    exports pl.polsl.lab.szymonbotor.notemanager.exceptions;
-    exports pl.polsl.lab.szymonbotor.notemanager.enums;
-}
---
+++ src/main/java/pl/polsl/lab/szymonbotor/notemanager/entities/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This is the entities package of the program.
+ * @author Szymon Botor
+ * @version 1.0
+ */
+package pl.polsl.lab.szymonbotor.notemanager.entities;
